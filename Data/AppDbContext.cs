5cf8a2933f742fcd6607f8136fbc251dda437049
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-04-27 10:03:52 +0200

    v2 initial

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatX.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ChatX.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
