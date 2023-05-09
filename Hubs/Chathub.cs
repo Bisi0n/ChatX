e58519567f79617875ce8a1177c552003cac8096
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-05-10 01:00:07 +0200

    Store message to db

--- Hubs/Chathub.cs
+++ Hubs/Chathub.cs
@@ -1,12 +1,19 @@
 ﻿using ChatX.Data;
 using ChatX.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 
 namespace ChatX.Hubs
 {
     public class Chathub : Hub
     {
         private static int _messageId = 0;
+        private readonly AppDbContext database;
+
+        public Chathub (AppDbContext context)
+        {
+            database = context;
+        }
 
         public async Task SendMessage(string loggedInUserName, int loggedInUser, string messageContent)
         {
@@ -19,7 +26,16 @@ namespace ChatX.Hubs
                 TimeStamp = DateTime.UtcNow
             };
 
-            // If we want to store messages, add them here to a db
+            // Save message to db
+            History messageHistory = new()
+            {
+                MessageId = Interlocked.Increment(ref _messageId),
+                User = loggedInUserName,
+                Content = messageContent,
+                TimeStamp = DateTime.UtcNow
+            };
+            database.Historys.Add(messageHistory);
+            await database.SaveChangesAsync();
 
             await Clients.All.SendAsync("ReceiveMessage", message);
         }
@@ -27,7 +43,8 @@ namespace ChatX.Hubs
         public async Task DeleteMessage(int id)
         {
             // Delete from db here
-
+            // Counsult with Customer if to keep/delete the message history
+            
             await Clients.All.SendAsync("deleteMessageRemote", id);
         }
     }
---
+++ Models/History.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatX.Models
+{
+    public class History
+    {
+        public int MessageId { get; set; }
+        public string Sender { get ; set; }
+        public string SenderName { get; set; }
+        public DateTime TimeStamp { get; set; }
+
+
+    }
+}
