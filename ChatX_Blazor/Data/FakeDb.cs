8f4cb32b794866f67f28b3f2d469ce4f1bfc36a3
Bisi0n <110070415+Bisi0n@users.noreply.github.com>
2023-06-14 20:14:56 +0200

    ChatX Blazor mini-edition

---
+++ ChatX_Blazor/Data/FakeDb.cs
@@ -0,0 +1,25 @@
+﻿using ChatX_Blazor.Models;
+
+namespace ChatX_Blazor.Data
+{
+    public class FakeDb
+    {
+        private List<Message> _messages = new ();
+        private int _idCount = 0;
+
+        public List<Message> GetAllMessages()
+        {
+            return _messages;
+        }
+
+        public void AddMessage(Message message)
+        {
+            message.Id = _idCount++;
+            _messages.Add(message);
+        }
+        public void RemoveMessage(Message message)
+        {
+            _messages.Remove(message);
+        }
+    }
+}
---
+++ ChatX_Blazor/Hubs/ChatHub.cs
@@ -0,0 +1,71 @@
+﻿using ChatX_Blazor.Data;
+using ChatX_Blazor.Models;
+using Microsoft.AspNetCore.SignalR;
+using System.Linq;
+using System.Security.Principal;
+
+namespace ChatX_Blazor.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private readonly FakeDb _db;
+        List<string> _usersCurrentlyTyping;
+
+        public ChatHub(FakeDb database)
+        {
+            _db = database;
+            _usersCurrentlyTyping = new();
+        }
+
+        public async Task SendMessage(string sender, string messageContent)
+        {
+            
+            Message message = new()
+            {
+                Content = messageContent,
+                Sender = sender,
+                TimeStamp = DateTime.UtcNow,
+            };
+
+            _db.AddMessage(message);
+
+            await Clients.All.SendAsync("ReceiveMessage", message);
+        }
+
+        public async Task DeleteMessage(int id)
+        {
+            Message? message = _db.GetAllMessages().Where(m => m.Id == id).SingleOrDefault();
+
+            if (message is not null)
+            {
+                _db.RemoveMessage(message);
+            }
+
+            await Clients.All.SendAsync("DeleteMessage", id);
+        }
+
+        public async Task LoadPreviousMessages()
+        {
+            List<Message> messages = _db.GetAllMessages();
+
+            await Clients.Caller.SendAsync("ReceiveMessageHistory", messages);
+        }
+
+        public async Task UserTyping(string user, bool isTyping)
+        {
+            if (isTyping)
+            {
+                if (!_usersCurrentlyTyping.Contains(user))
+                {
+                    _usersCurrentlyTyping.Add(user);
+                }
+            }
+            else
+            {
+                _usersCurrentlyTyping.Remove(user);
+            }
+
+            await Clients.All.SendAsync("CurrentlyTyping", _usersCurrentlyTyping);
+        }
+    }
+}
